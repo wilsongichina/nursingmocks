5b4cd1a52a6ab650ff3ae27938dc4dd986ca04da
AzmeerHamasAli <apnawaqtaiga@gmail.com>
2025-07-01 21:27:51 +0100

    fixing build error v2

--- src/components/sections/HowItWorksSection.tsx
+++ src/components/sections/HowItWorksSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import Link from "next/link";
 
 export default function HowItWorksSection() {
